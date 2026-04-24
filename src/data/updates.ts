f2c89646db3038f8a2f0053141718d9096288f64
agurjar123 <arjungurjar2006@gmail.com>
2026-04-23 23:03:03 -0700

    Reorder updates chronologically (oldest first)

--- src/data/updates.ts
+++ src/data/updates.ts
@@ -6,6 +6,19 @@ export type Update = {
 
 export const updates: Update[] = [
   {
+    date: "Jan 2025",
+    text: "Started undergraduate research at the Innovative Genomics Institute (IGI), UC Berkeley.",
+  },
+  {
+    date: "Mar 2025",
+    text: "Won Y Combinator Bio/AI Hackathon with tri-modal LUAD clinical decision support system.",
+    link: "https://github.com/agurjar123/yc-aixbio-hackathon",
+  },
+  {
+    date: "Nov 2025",
+    text: "Dendritic cell T cell priming work accepted to SITC 2025.",
+  },
+  {
     date: "Jan 2026",
     text: "Started teaching the Computational Biology bootcamp at UC Berkeley.",
   },
@@ -16,18 +29,5 @@ export const updates: Update[] = [
   {
     date: "Feb 2026",
     text: "T cell priming CRISPR screen work accepted for presentation at the ARC Symposium.",
-  },
-  {
-    date: "Nov 2025",
-    text: "Dendritic cell T cell priming work accepted to SITC 2025.",
-  },
-  {
-    date: "Mar 2025",
-    text: "Won Y Combinator Bio/AI Hackathon with tri-modal LUAD clinical decision support system.",
-    link: "https://github.com/agurjar123/yc-aixbio-hackathon",
-  },
-  {
-    date: "Jan 2025",
-    text: "Started undergraduate research at the Innovative Genomics Institute (IGI), UC Berkeley.",
   },
 ];
