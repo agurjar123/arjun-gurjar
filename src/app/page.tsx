b204a909325809a878d7cfa3d7e703c901ab70ff
agurjar123 <arjungurjar2006@gmail.com>
2026-04-23 22:22:42 -0700

    Remove QuickLinks from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Container from "@/components/ui/Container";
 import Hero from "@/components/home/Hero";
-import QuickLinks from "@/components/home/QuickLinks";
 
 export default function Home() {
   return (
     <Container>
       <Hero />
-      <QuickLinks />
     </Container>
   );
 }
