1dcdfc0f3f371e98fa3f4cfd815260af7373b50c
agurjar123 <arjungurjar2006@gmail.com>
2026-08-04 14:41:17 -0700

    Day 5: release early (alwaysOpen)

--- src/data/backstage/days.ts
+++ src/data/backstage/days.ts
@@ -128,6 +128,7 @@ Have a safe flight babyy and I’ll talk to you soon`,
     date: "2026-08-05",
     title: "T-minus 12",
     crypticAnswer: "bigben",
+    alwaysOpen: true, // released early for her
     content: {
       type: "friends",
       intro:
